3c01f8addcace545a20e85bd09074ab7edfee6b8
Heitor R. Barros <heitorrbarros@gmail.com>
2017-10-30 16:57:19 -0200

    Adicionar arquivos de projeto.

---
+++ ServiceDesk/Infrastructure/Infra.NHibernate/NHConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+
+namespace ServiceDesk.API.Infrastructure.Infra.NHibernate
+{
+    public class NHConfiguration
+    {
+        public ISessionFactory SessionFactory() => sessionFactory.Value;
+
+        static Lazy<ISessionFactory> sessionFactory = new Lazy<ISessionFactory>(() =>
+        {
+            var cfg = new Configuration();
+            cfg.Configure();
+
+            return cfg.BuildSessionFactory();
+        });
+    }
+}
---
+++ ServiceDesk/Infrastructure/Infra.NHibernate/TransactionBootstrap.cs
@@ -0,0 +1,38 @@
+﻿using NHibernate;
+using System;
+
+namespace ServiceDesk.API.Infrastructure.Infra.NHibernate
+{
+    public class TransactionBootstrap
+    {
+        private readonly ISession session;
+        private ITransaction transaction;
+
+        public TransactionBootstrap(ISession session) => this.session = session;
+        public void Begin()
+        {
+            if (transaction != null)
+                throw new InvalidOperationException("Já existe uma transação iniciada!");
+
+            if (transaction == null)
+                transaction = session.BeginTransaction();
+
+        }
+        public void Commit()
+        {
+            try
+            {
+                if (transaction == null)
+                    throw new InvalidOperationException("Você deve iniciar uma transação antes de confirma-lá!");
+
+                transaction.Commit();
+            }
+            catch (Exception ex)
+            {
+                transaction.Rollback();
+                ex = ex.InnerException ?? ex;
+                throw ex;
+            }
+        }
+    }
+}
